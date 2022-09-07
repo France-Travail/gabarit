3ae5ac2f00cc1633c58833126a3f1f11a30a6a6b
BAO Eva-ext <eva.bao-ext@pole-emploi.fr>
2022-09-07 17:10:05 +0200

    del save and reload super doucments in gbt, change unittest reload super documents

--- gabarit/template_nlp/nlp_project/package_name/models_training/model_tfidf_gbt.py
+++ gabarit/template_nlp/nlp_project/package_name/models_training/model_tfidf_gbt.py
@@ -134,8 +134,6 @@ class ModelTfidfGbt(ModelPipeline):
         Kwargs:
             configuration_path (str): Path to configuration file
             sklearn_pipeline_path (str): Path to standalone pipeline
-            count_vectorizer_path (str): Path to countVectorizer (only with_super_documents = True)
-            tfidf_super_documents_path (str): Path to tfidf super documents (only with_super_documents = True)
         Raises:
             ValueError: If configuration_path is None
             ValueError: If sklearn_pipeline_path is None
@@ -145,8 +143,6 @@ class ModelTfidfGbt(ModelPipeline):
         # Retrieve args
         configuration_path = kwargs.get('configuration_path', None)
         sklearn_pipeline_path = kwargs.get('sklearn_pipeline_path', None)
-        count_vectorizer_path = kwargs.get('count_vectorizer_path', None)
-        tfidf_super_documents_path = kwargs.get('tfidf_super_documents_path', None)
 
         # Checks
         if configuration_path is None:
@@ -167,13 +163,6 @@ class ModelTfidfGbt(ModelPipeline):
             configs['dict_classes'] = {int(k): v for k, v in configs['dict_classes'].items()}
         elif 'list_classes' in configs.keys():
             configs['dict_classes'] = {i: col for i, col in enumerate(configs['list_classes'])}
-
-        # Get the path of the super document if it is not given
-        dir = os.path.split(sklearn_pipeline_path)[:-1]
-        if count_vectorizer_path == None:
-            count_vectorizer_path = os.path.join(dir[0], f"count_vectorizer.pkl")
-        if tfidf_super_documents_path == None:
-            tfidf_super_documents_path = os.path.join(dir[0], "tfidf_super_documents.pkl")
 
         # Set class vars
         # self.model_name = # Keep the created name
@@ -198,10 +187,6 @@ class ModelTfidfGbt(ModelPipeline):
             self.gbt = self.pipeline['gbt']
         else:
             self.gbt = self.pipeline['gbt'].estimator
-
-        # Reload utile super documents
-        if self.with_super_documents:
-            self.tfidf.reload_from_standalone(count_vectorizer_path=count_vectorizer_path, tfidf_super_documents_path=tfidf_super_documents_path)
 
 
 if __name__ == '__main__':
