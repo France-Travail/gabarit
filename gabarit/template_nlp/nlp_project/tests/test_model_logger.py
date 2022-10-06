7ef0d81674adb6f94038379721952bce3fca836f
A_GAREL <alexandre.garel.56@gmail.com>
2022-10-06 11:56:10 +0200

    Gabarit (update): mini rework tests mlflow_logger

--- gabarit/template_nlp/nlp_project/tests/test_model_logger.py
+++ gabarit/template_nlp/nlp_project/tests/test_model_logger.py
@@ -21,6 +21,8 @@ import unittest
 import os
 import shutil
 import mlflow
+import numpy as np
+import pandas as pd
 
 from {{package_name}}.monitoring.mlflow_logger import MLflowLogger
 
@@ -28,95 +30,135 @@ from {{package_name}}.monitoring.mlflow_logger import MLflowLogger
 import logging
 logging.disable(logging.CRITICAL)
 
-TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+# TMP directory for mlruns
+MLRUNS_TMP_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp_experiments', 'mlruns')
+LOCAL_TRACKING_URI = f"file://{MLRUNS_TMP_DIR}"
+
 
 class MLflowLoggerTests(unittest.TestCase):
     '''Main class to test mlflow_logger'''
-    
+
     @classmethod
     def setUpClass(cls):
-        mlruns = os.path.join(TEST_DIR, "mlruns")
+        # Set mlruns directory
+        if os.path.exists(MLRUNS_TMP_DIR):
+            shutil.rmtree(MLRUNS_TMP_DIR)
 
-        if os.path.exists(mlruns):
-            shutil.rmtree(mlruns)
+    @classmethod
+    def tearDownClass(cls):
+        # Remove mlruns directory
+        if os.path.exists(MLRUNS_TMP_DIR):
+            shutil.rmtree(MLRUNS_TMP_DIR)
 
-        mlflow.set_tracking_uri(f"file://{mlruns}")
-        
+    def setUp(self):
+        '''SetUp fonction'''
+        # Change directory to script directory
+        abspath = os.path.abspath(__file__)
+        dname = os.path.dirname(abspath)
+        os.chdir(dname)
 
-    def test_mlflow_logger_init(self):
+    def test01_mlflow_logger_init(self):
         '''Test of the initialization of {{package_name}}.monitoring.mlflow_logger.MLflowLogger'''
         experiment_name = 'test_mlflow_logger_init'
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
+        self.assertEqual(mlflow_logger.tracking_uri, LOCAL_TRACKING_URI)
+        self.assertEqual(mlflow.get_tracking_uri(), LOCAL_TRACKING_URI)
+        self.assertEqual(mlflow_logger.experiment_name, experiment_name)
+        # No need to call end_run
 
-        model = MLflowLogger(experiment_name=experiment_name)
-        self.assertEqual(model.experiment_name, experiment_name)
-
-    def test_mlflow_logger_stop_run(self):
-        '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.stop_run'''
-        experiment_name = 'test_mlflow_logger_stop_run'
-        model = MLflowLogger(experiment_name=experiment_name)
-
+    def test02_mlflow_logger_end_run(self):
+        '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.end_run'''
+        experiment_name = 'test_mlflow_logger_end_run'
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
         # We activate a run via a log
-        model.log_param('stop', 'toto')
-
-        # Use of stop_run
-        model.stop_run()
-
-        # Check
+        mlflow_logger.log_param('stop', 'toto')
+        # Use of end_run & check
+        mlflow_logger.end_run()
         self.assertEqual(mlflow.active_run(), None)
 
-    def test_mlflow_logger_log_metric(self):
+    def test03_mlflow_logger_log_metric(self):
         '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.log_metric'''
         experiment_name = 'test_mlflow_logger_log_metric'
-        model = MLflowLogger(experiment_name=experiment_name)
-
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
         # Nominal case
-        model.log_metric('test', 5)
-        model.log_metric('test', 5, step=2)
-
+        mlflow_logger.log_metric('test', 5)
+        mlflow_logger.log_metric('test', 5, step=2)
         # Clear
-        model.stop_run()
+        mlflow_logger.end_run()
 
-    def test_mlflow_logger_log_metrics(self):
+    def test04_mlflow_logger_log_metrics(self):
         '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.log_metrics'''
         experiment_name = 'test_mlflow_logger_log_metrics'
-        model = MLflowLogger(experiment_name=experiment_name)
-
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
         # Nominal case
-        model.log_metrics({'test': 5})
-        model.log_metrics({'test': 5}, step=2)
+        mlflow_logger.log_metrics({'test': 5})
+        mlflow_logger.log_metrics({'test': 5}, step=2)
+        # Clear
+        mlflow_logger.end_run()
 
-    def test_mlflow_logger_log_param(self):
+    def test05_mlflow_logger_log_param(self):
         '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.log_param'''
         experiment_name = 'test_mlflow_logger_log_param'
-        model = MLflowLogger(experiment_name=experiment_name)
-
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
         # Nominal case
-        model.log_param('test', 5)
+        mlflow_logger.log_param('test', 5)
+        # Clear
+        mlflow_logger.end_run()
 
-    def test_mlflow_logger_log_params(self):
-        '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.log_params'''    
+    def test06_mlflow_logger_log_params(self):
+        '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.log_params'''
         experiment_name = 'test_mlflow_logger_log_params'
-        model = MLflowLogger(experiment_name=experiment_name)
-
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
         # Nominal case
-        model.log_params({'test': 5})
+        mlflow_logger.log_params({'test': 5})
+        # Clear
+        mlflow_logger.end_run()
 
-    def test_mlflow_logger_set_tag(self):
+    def test07_mlflow_logger_set_tag(self):
         '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.set_tag'''
         experiment_name = 'test_mlflow_logger_set_tag'
-        model = MLflowLogger(experiment_name=experiment_name)
-
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
         # Nominal case
-        model.set_tag('test', 5)
+        mlflow_logger.set_tag('test', 5)
+        # Clear
+        mlflow_logger.end_run()
 
-    def test_mlflow_logger_set_tags(self):
+    def test08_mlflow_logger_set_tags(self):
         '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.set_tags'''
         experiment_name = 'test_mlflow_logger_set_tags'
-        model = MLflowLogger(experiment_name=experiment_name)
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
+        # Nominal case
+        mlflow_logger.set_tags({'test': 5})
+        # Clear
+        mlflow_logger.end_run()
+
+    def test09_mlflow_logger_valid_name(self):
+        '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.valid_name'''
+        experiment_name = 'test_mlflow_logger_valid_name'
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
+        # Valid case
+        self.assertTrue(mlflow_logger.valid_name('valid_name'))
+        self.assertFalse(mlflow_logger.valid_name('not a valid_name!'))
+        # No need to call end_run
+
+    def test10_mlflow_logger_log_df_stats(self):
+        '''Test of {{package_name}}.monitoring.mlflow_logger.MLflowLogger.log_df_stats'''
+        experiment_name = 'test_mlflow_logger_log_df_stats'
+        mlflow_logger = MLflowLogger(experiment_name=experiment_name, tracking_uri=LOCAL_TRACKING_URI)
 
         # Nominal case
-        model.set_tags({'test': 5})
+        df_stats = pd.DataFrame({
+            'Label': ['label1', 'label2', 'label3', 'label4'],
+            'metric1': [0.5, 0.2, 1.0, -1.5],
+            'metric2!!!': [None, 0.2, 'test', np.NaN]
+        })
+        mlflow.log_df_stats(df_stats)
 
+        # Clear
+        mlflow_logger.end_run()
+
+# TODO: log_dict & log_text
+# TODO: check local writing ?
 
 # Perform tests
 if __name__ == '__main__':
