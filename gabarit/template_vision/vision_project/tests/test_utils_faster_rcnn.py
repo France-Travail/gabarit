4c86b8a43e8e1af66c55384f41069e462fc8e37d
hakim <hakim.cheikh-ext@pole-emploi.fr>
2023-01-06 16:56:39 +0100

    minor: remove add tests TODO

--- gabarit/template_vision/vision_project/tests/test_utils_faster_rcnn.py
+++ gabarit/template_vision/vision_project/tests/test_utils_faster_rcnn.py
@@ -14,8 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# TODO : There are some tests missing,
-
 # Libs unittest
 import unittest
 from unittest.mock import Mock
