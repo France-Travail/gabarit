fa45f09930df79910f3ea76e54c4eea247f809df
Jean-Baptiste <jeanbapt.martin@gmail.com>
2024-02-15 13:33:41 +0000

    :zap: use of mimalloc + improve logging for uvicorn

--- gabarit/template_api/api_project/launch.sh
+++ gabarit/template_api/api_project/launch.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-#
-export option="$1"
+#!/usr/bin/env sh
+
+export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libmimalloc.so.2"
+export LD_BIND_NOW=1
+
+: "${HOST:=0.0.0.0}"
+: "${PORT:=5000}"
+: "${WORKERS:=1}"
 
 download-model
-uvicorn --host 0.0.0.0 --port 5000 {{package_name}}.application:app ${option}
+
+uvicorn {{package_name}}.application:app \
+  --log-config log_conf.json \
+  --host $HOST \
+  --port $PORT \
+  --workers $WORKERS
