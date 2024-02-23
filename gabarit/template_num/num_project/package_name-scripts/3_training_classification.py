635b4b35dc9c8c00c8de2440c55b59af8ea46438
Daniel <673998+dgarlor@users.noreply.github.com>
2024-02-23 08:15:59 +0100

    Correct a type error (#195)
    * Update 2_training.py
    
    Type error
    
    * type chaned
    
    * Update num 3_training_regression.py
    
    * Update 3_predict.py
    
    * Update 4_predict.py

--- gabarit/template_num/num_project/package_name-scripts/3_training_classification.py
+++ gabarit/template_num/num_project/package_name-scripts/3_training_classification.py
@@ -376,7 +376,7 @@ def main(filename: str, y_col: List[Union[str, int]], excluded_cols: Union[List[
     ##############################################
 
     # Series to add
-    cols_to_add: List[pd.Series] = []  # You can add columns to save here
+    cols_to_add: List[str] = []  # You can add columns to save here
     series_to_add_train = [df_train[col] for col in cols_to_add]
     series_to_add_valid = [df_valid[col] for col in cols_to_add]
     gc.collect()  # In some cases, helps with OOMs
